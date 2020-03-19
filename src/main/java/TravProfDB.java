36e4a5652c48eff3529b47e0fddefe6e21b72101
Cristian Gutu <gutucristian27@gmail.com>
2020-03-19 19:22:45 -0400

    add core functionality and clean up

--- src/main/java/TravProfDB.java
+++ src/main/java/TravProfDB.java
@@ -5,8 +5,8 @@ import java.io.*;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TravProfDB {
@@ -19,16 +19,35 @@ public class TravProfDB {
 
     public TravProfDB(String path) throws IOException {
         this.PATH = path;
-        this.travelerList = new ArrayList<>();
+        this.travelerList = new LinkedList<>();
         this.gson = new Gson();
-        /*
-        this.travelerList = Arrays.asList(
-                new TravProf("1", "John", "Doe", "20 Post Rd",
-                        "203-345-2341", 456, "Pleasure", "Credit", null),
-                new TravProf("2", "Bob", "Smith", "25 Flat Rock",
-                        "203-345-2321", 1023, "Pleasure", "Debit", null)
-        );
-        */
+    }
+
+    public void insertNewProfile(TravProf profile) { this.travelerList.add(profile); }
+
+    public boolean deleteProfile(String travAgentId, String lastName) {
+        int travProfileIndex = getTravProfileIndex(travAgentId, lastName);
+        if (travProfileIndex >= 0) {
+            this.travelerList.remove(travProfileIndex);
+            return true;
+        }
+        return false;
+    }
+
+    public TravProf findProfile(String travAgentId, String lastName) {
+        int travProfileIndex = getTravProfileIndex(travAgentId, lastName);
+        return travProfileIndex == -1 ? null : this.travelerList.get(travProfileIndex);
+    }
+
+    private int getTravProfileIndex(String travAgentId, String lastName) {
+        int i = 0;
+        for (TravProf profile: this.travelerList) {
+            if (profile.getTravAgentID().equals(travAgentId)
+                    && profile.getLastName().equals(lastName))
+                return i;
+            ++i;
+        }
+        return -1;
     }
 
     public void writeAllTravProf(String path) throws IOException {
