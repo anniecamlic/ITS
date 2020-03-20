51648f3d756ba6bb8c25ede14fb105987734ea0e
Cristian Gutu <gutucristian27@gmail.com>
2020-03-20 14:36:54 -0400

    add toString

--- src/main/java/MedCond.java
+++ src/main/java/MedCond.java
@@ -14,25 +14,37 @@ public class MedCond {
     public String getMdContact(){
         return this.mdContact;
     }
+
     public String getMdPhone(){
         return this.mdPhone;
     }
+
     public String getAlgType(){
         return this.algType;
     }
+
     public String getIllType(){
         return this.illType;
     }
+
     public void updateMdContact(String newMDC){
         this.mdContact = newMDC;
     }
+
     public void updateMdPhone(String newPhone){
         this.mdPhone = newPhone;
     }
+
     public void updateAlgType(String newAlgType){
         this.algType = newAlgType;
     }
+
     public void updateIllType(String newIllType){
         this.illType = newIllType;
+    }
+
+    public String toString() {
+        return "Contact: " + this.mdContact + " (" + this.mdPhone + ")" +
+                ", AlgType: " + this.algType + ", IllType: " + this.illType;
     }
 }
