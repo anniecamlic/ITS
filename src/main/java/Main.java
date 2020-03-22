6926d406bb01fccb032c770336093804a87f45bc
Cristian Gutu <gutucristian27@gmail.com>
2020-03-22 17:07:24 -0400

    remove log

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("started...");
         TravProfInterface app = new TravProfInterface("db.json");
         app.run();
     }
