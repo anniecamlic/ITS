2e1028aafa4058c71e653e8b73e7d5f0f28c488a
anniecamlic <anniecamlic@gmail.com>
2020-03-25 13:18:04 -0400

    documented classes
    I documented the MedCond and TravProf classes. I can also document TravProfInterface, but i hesitate because it is not completed yet.

--- src/main/java/MedCond.java
+++ src/main/java/MedCond.java
@@ -4,6 +4,17 @@ public class MedCond {
     private String algType;
     private String illType;
 
+    /**
+     * MedCond Constructor
+     * Creates an instance of the MedCond Class
+     *
+     * @param mdContact - medical contact saved as  a string
+     * @param mdPhone - medical contact's phone number saved as a string
+     * @param algType - allergy type saved as a string
+     * @param illType - illness type saved as a string
+     *
+     * @return instance of the MedCond class
+     */
     public MedCond(String mdContact,String mdPhone, String algType, String illType){
         this.mdContact = mdContact;
         this.mdPhone = mdPhone;
@@ -11,38 +22,70 @@ public class MedCond {
         this.illType = illType;
     }
 
+    /**
+     * returns the MedCond object's medical contact
+     */
     public String getMdContact(){
         return this.mdContact;
     }
 
+    /**
+     * returns the MedCond object's medical contact phone number
+     */
     public String getMdPhone(){
         return this.mdPhone;
     }
 
+    /**
+     * returns the MedCond object's allergy type
+     */
     public String getAlgType(){
         return this.algType;
     }
 
+    /**
+     * returns the MedCond object's illness type
+     */
     public String getIllType(){
         return this.illType;
     }
 
+
+    /**
+     * updates the MedCond object's medical contact
+     */
     public void updateMdContact(String newMDC){
         this.mdContact = newMDC;
     }
 
+    /**
+     * updates the MedCond object's medical contact phone number
+     */
     public void updateMdPhone(String newPhone){
         this.mdPhone = newPhone;
     }
 
+    /**
+     * updates the MedCond object's allergy type
+     */
     public void updateAlgType(String newAlgType){
         this.algType = newAlgType;
     }
 
+    /**
+     * updates the MedCond object's illness type
+     */
     public void updateIllType(String newIllType){
         this.illType = newIllType;
     }
 
+
+    /**
+     * converts the MedCond object to a string
+     *
+     *
+     * @return a MedCond instance in string form
+     */
     public String toString() {
         return "Contact: " + this.mdContact + " (" + this.mdPhone + ")" +
                 ", AlgType: " + this.algType + ", IllType: " + this.illType;
--- src/main/java/TravProf.java
+++ src/main/java/TravProf.java
@@ -9,6 +9,21 @@ public class TravProf {
     private String travelType;
     private String paymentType;
 
+    /**
+     * TravProf Constructor
+     * Creates an instance of the TravProf Class
+     *
+     * @param travelAgentID - travel agent ID saved as a string
+     * @param firstName - first name saved as a string
+     * @param lastName - last name saved as a string
+     * @param address - phone number saved as a string
+     * @param tripCost - cost of trip saved as a doubly
+     * @param travelType - travel type saved as a string
+     * @param paymentType - payment type saved as a string
+     * @param medCondInfo - medical condition information saved as an instance of the MedCond class
+     *
+     * @return instance of the TravProf class
+     */
     public TravProf(String travelAgentID, String firstName, String lastName, String address,
                     String phone, double tripCost, String travelType, String paymentType, MedCond medCondInfo){
         this.travelAgentID = travelAgentID;
@@ -21,45 +36,108 @@ public class TravProf {
         this.paymentType = paymentType;
         this.medCondInfo = medCondInfo;
     }
+
+    /**
+     * returns the TravProf object's travel agent ID
+     */
     public String getTravAgentID(){ return this.travelAgentID; }
 
+    /**
+     * returns the TravProf object's first name
+     */
     public String getFirstName(){ return this.firstName; }
 
+    /**
+     * returns the TravProf object's last name
+     */
     public String getLastName(){ return this.lastName; }
 
+    /**
+     * returns the TravProf object's address
+     */
     public String getAddress(){ return this.address; }
 
+    /**
+     * returns the TravProf object's phone number
+     */
     public String getPhone(){ return this.phone; }
 
+    /**
+     * returns the TravProf object's trip cost
+     */
     public double getTripCost(){ return this.tripCost; }
 
+    /**
+     * returns the TravProf object's travel type
+     */
     public String getTravelType(){ return this.travelType; }
 
+    /**
+     * returns the TravProf object's payment type
+     */
     public String getPaymentType(){ return this.paymentType; }
 
+    /**
+     * returns the TravProf object's medical condition information
+     */
     public MedCond getMedCondInfo(){ return this.medCondInfo; }
 
+    /**
+     * updates the TravProf object's first name
+     */
     public void updateFirstName(String newName){ this.firstName = newName; }
 
+    /**
+     * updates the TravProf object's last name
+     */
     public void updateLastName(String newName){ this.lastName = newName; }
 
+    /**
+     * updates the TravProf object's address
+     */
     public void updateAddress(String newAddress){ this.address = newAddress; }
 
+    /**
+     * updates the TravProf object's phone number
+     */
     public void updatePhone(String newPhone){ this.phone = newPhone; }
 
+    /**
+     * updates the TravProf object's trip cose
+     */
     public void updateTripCost(Float newCost){ this.tripCost = newCost; }
 
+    /**
+     * updates the TravProf object's travel type
+     */
     public void updateTravelType(String newTType){ this.travelType = newTType; }
 
+    /**
+     * updates the TravProf object's payment type
+     */
     public void updatePaymentType(String newPType){ this.paymentType = newPType; }
 
+    /**
+     * updates the TravProf object's medical condition information
+     */
     public void updateMedCondInfo(MedCond newMC){ this.medCondInfo = newMC; }
 
+    /**
+     * converts the TravProf object to a string
+     *
+     * @return a TravProf instance in string form
+     */
     public String toString() {
         return this.firstName + " " + this.lastName +
                 (this.medCondInfo != null ? " [" + this.medCondInfo.toString() + "]" : "");
     }
 
+    /**
+     * compares two objects to see if they are equivalent
+     *
+     * @param o - this is an object being compared
+     * @return a boolean value that described whether or not two objects are equal
+     */
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof TravProf)) return false;
